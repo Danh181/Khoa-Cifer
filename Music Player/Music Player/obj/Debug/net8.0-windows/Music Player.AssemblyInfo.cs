caa0ce6322d08d86e26b3023e96c48b98a7a4074
Danh181 <danhthanh18102004@gmail.com>
2024-10-27 22:36:47 +0700

    checkV3

--- Music Player/Music Player/obj/Debug/net8.0-windows/Music Player.AssemblyInfo.cs
+++ Music Player/Music Player/obj/Debug/net8.0-windows/Music Player.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Music Player")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597e12da53f7a5035739509b98a5401f4ab0e635")]
 [assembly: System.Reflection.AssemblyProductAttribute("Music Player")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Music Player")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
